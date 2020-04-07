8f3005515c9618c56658ccf80d18b6d495db46cd
te17adrian <adrian.lidgren@elev.ga.ntig.se>
2020-04-07 11:11:17 +0200

    Filer från förra projektet

---
+++ src/solomodel.java
@@ -0,0 +1,2 @@
+public class solomodel {
+}
---
+++ src/soloview.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class soloview {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JTextArea textArea1;
+    private JTextField textField1;
+}
