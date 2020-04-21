f132730165f132152e2c0790e6dd2043f55d6ea7
te17adrian <adrian.lidgren@elev.ga.ntig.se>
2020-04-21 10:28:42 +0200

    controller

--- src/solocontroller.java
+++ src/solocontroller.java
@@ -1,2 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class solocontroller {
+    private soloview Theview;
+    private solomodel Themodel;
+
+public solocontroller(soloview theview, solomodel themodel) {
+    this.Theview = theview;
+    this.Themodel = themodel;
+    this.Theview.addsoloListener(new sololistener());
+}
+
 }
