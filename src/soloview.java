3adcbd2a7b55ab0aeb9c64726bcde994b2b3c7d3
te17adrian <adrian.lidgren@elev.ga.ntig.se>
2020-05-12 11:22:09 +0200

    V.20

--- src/soloview.java
+++ src/soloview.java
@@ -1,10 +1,17 @@
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class soloview {
+public class soloview extends JFrame {
     private JPanel panel1;
     private JButton option1;
     private JButton option2;
     private JTextArea story;
-    private JTextField Story_name;
+
+    soloview() {
+        JPanel soloPanel = new JPanel();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 400);
+        soloPanel.add(story);
+
+    }
 }
